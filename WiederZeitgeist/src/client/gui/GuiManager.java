218cf3d6b67ce67c977323a1a7bb9aec44d376a6
TARS <TARS@LAPTOP-7EJ12RQB>
2019-05-01 16:51:30 -0700

    Added a null-check in GuiManager to avoid null-bugs.

--- WiederZeitgeist/src/client/gui/GuiManager.java
+++ WiederZeitgeist/src/client/gui/GuiManager.java
@@ -12,56 +12,64 @@ import java.util.TreeSet;
 
 /**
  * The singleton head for passing and managing the gui structure.
+ *
  * @author TARS
  */
 public class GuiManager {
-    
+
     /**
      * The main instance of GuiManager
      */
     public static final GuiManager GUI_MANAGER = new GuiManager();
-    
+
     private Component currentComponent = null;
     private final Set<Class<? extends Message>> typesToPass;
-    
+
     private GuiManager() {
         typesToPass = new TreeSet();
     }
-    
+
     /**
      * Returns the current component of the manager.
+     *
      * @return The current component.
      */
-    public Component getCurrentComponent(){
+    public Component getCurrentComponent() {
         return currentComponent;
     }
-    
+
     /**
      * Adds the message type to the list of types to be passed on to components.
+     *
      * @param <M> The message type;
      * @param messageType The message type;
      */
-    public <M extends Message> void passMessageType(Class<M> messageType){
-        if(!typesToPass.contains(messageType)){
-            CLIENT_LISTENER.addListener(messageType, m -> currentComponent.handleMessage(m));
+    public <M extends Message> void passMessageType(Class<M> messageType) {
+        if (!typesToPass.contains(messageType)) {
+            CLIENT_LISTENER.addListener(messageType, m -> {
+                if (currentComponent != null) {
+                    currentComponent.handleMessage(m);
+                }
+            });
             typesToPass.add(messageType);
         }
     }
-    
+
     /**
      * Renders the manager's component.
      */
     public void render() {
-        if(currentComponent != null){
+        if (currentComponent != null) {
             currentComponent.render();
         }
     }
-    
+
     /**
      * Sets the current component of the manager.
+     *
      * @param comp the component to set as the current.
      */
-    public void setCurrentComponent(Component comp){
+    public void setCurrentComponent(Component comp) {
         currentComponent = comp;
     }
 }
